e909e5404e5891ff8d8832250bda082d3c244c60
Oxylium-jpg <79831561+Oxylium-jpg@users.noreply.github.com>
2022-12-07 19:30:38 +0100

    Salut j'ai tout changé mais on a un super header et des super routes

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Header/header.jsx";
+import Footer from "../Footer/footer.jsx";
 
 function NotFound() {
   return (
@@ -7,25 +8,7 @@ function NotFound() {
       <Header />
       <h1>NOT FOUND</h1>
       <h2>Vite + React + TS (Hamburger + Responsive + Router)</h2>
-      <footer className="footer">
-        <p className="footer-by">
-          A project by{" "}
-          <a
-            href="https://twitter.com/erdemmkarakas"
-            rel="noopener"
-            className="small-link"
-          >
-            Erdem Karakaş
-          </a>
-          <a
-            href="https://twitter.com/erdemmkarakas"
-            rel="noopener"
-            target="_blank"
-            className="no-link icon-twitter"
-            aria-label="Follow me on Twitter"
-          ></a>
-        </p>
-      </footer>
+      <Footer/>
     </body>
   );
 }
