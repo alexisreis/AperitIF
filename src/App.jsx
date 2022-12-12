4d00eb578a490d76caa3ecc038587c436a9d68a5
ThomasColin1 <saumontomate@gmail.com>
2022-12-12 13:03:07 +0100

    Réglages PR de mael

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import React from "react";
 import Header from "./components/Header/header.jsx";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/home.jsx";
