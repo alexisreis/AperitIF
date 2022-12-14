8d01d6f52ebd1775fa22365c0c712f6a46432e30
alexis <alexisreis@outlook.fr>
2022-12-14 09:16:28 +0100

    SearchBar pushed

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { Search, Option, Detail } from "searchpal";
 import React, {useState} from "react";
+import "./SearchBar.scss"
 
 const SearchBar = ({ cocktails, session }) => {
 
@@ -7,7 +8,27 @@ const SearchBar = ({ cocktails, session }) => {
 
 	return (
 		<>
-			<button onClick={() => setOpen(true)}>Rechercher un cocktail</button>
+			{!open ? <div id="searchbar" onClick={() => setOpen(true)}>
+				<div id="searchbar-inner">
+					<svg viewBox="0 0 24 24" aria-hidden="true"
+					     xmlns="http://www.w3.org/2000/svg"
+					     id="searchbar-icon">
+						<path
+							d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
+					</svg>
+					<div id="searchbar-text-div">
+						<input type="text"
+						       placeholder="Rechercher un cocktail..."
+						       role="combobox" aria-expanded="false"
+						       aria-controls="298dc1ea-4e6b-43de-85b6-a533bb79cfe2-results"
+						       id="searchbar-label"
+						       autoComplete="off" className="sc-iTFTee dZgGse"
+						       value=""/>
+
+					</div>
+				</div>
+			</div> : null}
+
 			<Search
 				labels={{
 					results: "Cocktails trouvés",
@@ -27,7 +48,7 @@ const SearchBar = ({ cocktails, session }) => {
 						label={cocktail.name}
 						sublabel={cocktail.name}
 						img={{ src: cocktail.img, alt: `${cocktail.name} profile picture` }}
-						href={`/${cocktail.name}`}
+						href={`/#/cocktail/${cocktail.name}`}
 						keywords={(getKeywords) =>
 							getKeywords(
 								cocktail.name,
@@ -39,7 +60,7 @@ const SearchBar = ({ cocktails, session }) => {
 				))}
 			</Search>
 		</>
-	);
+	)
 };
 
 export default SearchBar;
