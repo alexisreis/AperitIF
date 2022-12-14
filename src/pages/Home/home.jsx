835ec118c1114e145892f3f3a7d000f243f3a07f
VegasRedfield <arrive.mael@gmail.com>
2022-12-14 10:06:23 +0100

    commit

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "../../components/Header/header.jsx";
 import Footer from "../../components/Footer/footer.jsx";
 import RequestExample from "../../components/RequestExample.jsx";
+import SearchBar from "../../components/SearchBar/SearchBar.jsx";
+import cocktails from "../../assets/cocktails.json"
 
 function Home() {
   return (
     <div>
       <Header />
       <h1>Home</h1>
+        <SearchBar cocktails={cocktails}/>
       <RequestExample/>
       <Footer/>
     </div>
